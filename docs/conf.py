81ea31df1b87e4d07f973a05d4360983e50ccb0f
Augustin Trancart <augustin.trancart@oslandia.com>
2025-01-20 11:58:27 +0100

    docs(sphinx): add a static css to customize theme

--- docs/conf.py
+++ docs/conf.py
@@ -117,16 +117,14 @@ html_theme_options = {
     "navbar_start": ["navbar-logo", "version-switcher"],
 }
 
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-# html_theme_options = {"display_version": True}
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+html_css_files = [
+    "css/custom.css",
+]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
