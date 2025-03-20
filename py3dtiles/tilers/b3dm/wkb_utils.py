fix(wkb_utils): change implicit overflow into an explict overflow
Note that this overflow is really worrying in itself. This commit merely
makes the code behaves the same way after numpy 2.1, it does not fix the
underlying issue (py3dtiles being unable to correctly triangulate
polygon with more than 256 vertices).

py3dtiles/tilers/b3dm/wkb_utils.py
@@ -28,7 +28,7 @@ class TriangleSoup:
     def triangle_indices(self) -> npt.NDArray[np.uint8]:
         """Express the triangles as triplets of vertex indices."""
         flattened_triangles = np.concatenate(self.triangles[0])
-        indices = np.full(flattened_triangles.shape[0], dtype=np.uint8, fill_value=-1)
+        indices = np.full(flattened_triangles.shape[0], dtype=np.uint8, fill_value=255)
         for vertex_idx in range(self.vertices.shape[0]):
             indices[
                 np.all(flattened_triangles == self.vertices[vertex_idx], axis=1)