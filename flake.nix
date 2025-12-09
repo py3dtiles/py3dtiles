add7fb6ab5e0678a199f32c0ad72a47545352dbf
Augustin Trancart <augustin.trancart@oslandia.com>
2025-12-09 11:04:52 +0100

    build: update flake & flake.lock

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "py3dtiles' flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
     flakeUtils = {
       type = "github";
       owner = "numtide";
